84185fb84c8ac5f216f378a9cbbe1fca6ece88a0
Thusiya <56513558+Thusiya05@users.noreply.github.com>
2020-05-24 10:25:20 +0530

    Changed

--- Assignment 1/Question3.scala
+++ Assignment 1/Question3.scala
@@ -16,7 +16,7 @@ object Question3 {
          
          //n= no of copies and n >=50
           var cost_of_books=n*24.95*0.6
-          var cost_for_shiping=3*50+(n-50)*0.75
+          var cost_for_shiping=3+(n-50)*0.75
           var wholesale_cost=cost_of_books+cost_for_shiping
           return wholesale_cost
           
